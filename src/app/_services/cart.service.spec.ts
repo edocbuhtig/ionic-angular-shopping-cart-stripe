ce4a68722fcf9594fa37a3cfaa38be09c6ab0cc8
Sunil Kumar <45172271+edocbuhtig@users.noreply.github.com>
2020-07-01 00:09:21 +0530

    Add files via upload

--- src/app/_services/cart.service.spec.ts
+++ src/app/_services/cart.service.spec.ts
@@ -3,10 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { CartService } from './cart.service';
 
 describe('CartService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: CartService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CartService);
+  });
 
   it('should be created', () => {
-    const service: CartService = TestBed.get(CartService);
     expect(service).toBeTruthy();
   });
 });
