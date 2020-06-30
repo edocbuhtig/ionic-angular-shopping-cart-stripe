3f2e498b984a94442b44ce61746dc184750dfcb8
Sunil Kumar <45172271+edocbuhtig@users.noreply.github.com>
2020-07-01 00:10:07 +0530

    Add files via upload

--- src/app/cartview/cartview.module.ts
+++ src/app/cartview/cartview.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
-import { CartviewPage } from './cartview.page';
+import { CartviewPageRoutingModule } from './cartview-routing.module';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: CartviewPage
-  }
-];
+import { CartviewPage } from './cartview.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    CartviewPageRoutingModule
   ],
   declarations: [CartviewPage]
 })
